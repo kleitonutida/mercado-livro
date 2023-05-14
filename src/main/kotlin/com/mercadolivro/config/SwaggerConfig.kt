333942acf10202c36e62882b4f572772bbdc8f1e
Kleiton Utida <kleiton.utida@live.com>
2023-05-14 00:08:09 -0300

    Customizando configurações do swagger

---
+++ src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
@@ -0,0 +1,19 @@
+package com.mercadolivro.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun api(): OpenAPI = OpenAPI()
+        .info(
+            Info()
+                .title("Mercado Livro")
+                .description("API do Mercado Livro")
+                .version("v0.0.1"),
+        )
+}
