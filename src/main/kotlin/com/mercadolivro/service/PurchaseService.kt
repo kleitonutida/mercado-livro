25eac5d094dab764245f4957aa4a3fa3ebdfebbf
Kleiton Utida <kleiton.utida@live.com>
2023-05-16 00:20:06 -0300

    Configuração e publicação de eventos

--- src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
+++ src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
@@ -1,10 +1,18 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.events.PurchaseEvent
 import com.mercadolivro.model.PurchaseModel
+import com.mercadolivro.repository.PurchaseRepository
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Service
 
-class PurchaseService {
-
+@Service
+class PurchaseService(
+    val purchaseRepository: PurchaseRepository,
+    val applicationEventPublisher: ApplicationEventPublisher,
+) {
     fun create(purchaseModel: PurchaseModel) {
-
+        purchaseRepository.save(purchaseModel)
+        applicationEventPublisher.publishEvent(PurchaseEvent(this, purchaseModel))
     }
 }
