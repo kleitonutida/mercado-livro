4ab80330d9d8dcbe674180131f32a5a079fb59c5
Kleiton Utida <kleiton.utida@live.com>
2023-06-03 02:39:53 -0300

    Complementando o teste e configurando o jacoco

--- src/test/kotlin/com/mercadolivro/service/CustomerServiceTest.kt
+++ src/test/kotlin/com/mercadolivro/service/CustomerServiceTest.kt
@@ -1,11 +1,11 @@
 package com.mercadolivro.service
 
-import com.mercadolivro.controller.response.PageResponse
 import com.mercadolivro.enums.CustomerStatus
 import com.mercadolivro.enums.Role
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.CustomerRepository
 import io.github.glytching.junit.extension.random.Random
+import io.github.glytching.junit.extension.random.RandomBeansExtension
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
@@ -14,14 +14,12 @@ import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
-import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import java.util.*
 
-@ExtendWith(MockKExtension::class)
+@ExtendWith(MockKExtension::class, RandomBeansExtension::class)
 class CustomerServiceTest {
 
     @MockK
@@ -37,17 +35,34 @@ class CustomerServiceTest {
     private lateinit var customerService: CustomerService
 
     @Test
-    fun `should return all customers`() {
-        val pageable = Pageable.ofSize(10).withPage(0)
-        val expectedPage = PageImpl(listOf(buildCustomer(), buildCustomer()))
+    fun `should return all customers`(
+        @Random pageable: Pageable,
+    ) {
+        val expectedCustomers = PageImpl(listOf(buildCustomer(), buildCustomer()))
 
-        every { customerRepository.findAll(pageable) } returns expectedPage
+        every { customerRepository.findAll(pageable) } returns expectedCustomers
 
-        val customers = customerService.getAll(pageable, null)
+        val actualCustomers = customerService.getAll(pageable, null)
 
-        assertEquals(expectedPage, customers)
+        assertEquals(expectedCustomers, actualCustomers)
         verify(exactly = 1) { customerRepository.findAll(pageable) }
         verify(exactly = 0) { customerRepository.findByNameContaining(any(), any()) }
+    }
+
+    @Test
+    fun `should return customers when name is informed`(
+        @Random pageable: Pageable,
+        @Random(size = 20) name: String,
+    ) {
+        val expectedCustomers = PageImpl(listOf(buildCustomer(), buildCustomer()))
+
+        every { customerRepository.findByNameContaining(pageable, name) } returns expectedCustomers
+
+        val actualCustomers = customerService.getAll(pageable, name)
+
+        assertEquals(expectedCustomers, actualCustomers)
+        verify(exactly = 0) { customerRepository.findAll(pageable) }
+        verify(exactly = 1) { customerRepository.findByNameContaining(any(), any()) }
     }
 
     fun buildCustomer(
